185a8e2e45fd0d4c333ef1221469d517375a87c1
AlvinNdei <alvinmuriuki714@gmail.com>
2026-05-20 18:13:24 +0300

    fixed alignment in the edit post page

--- resources/views/edit_post.blade.php
+++ resources/views/edit_post.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.layout')
-<div>
+<div style="border:3px solid black; background-color:gray;">
     <form action="{{ route('post.update', $post->id) }}" method="POST">
         @csrf
         @method('PUT')
+        <label>Title</label><br>
         <input type="text" name="title" value="{{$post->title}}">
-        <textarea name="body" >{{$post->body}}</textarea>
-        <button>Save changes</button>
+        <h3>Edit your post</h3>
+        <textarea name="body" >{{$post->body}}</textarea><br>
+        <button>Save</button>
     </form>
 </div>
